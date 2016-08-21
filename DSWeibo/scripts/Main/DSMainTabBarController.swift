e8d8acb8e8b55b8f17ad2a8877072498c8df53e5
SendSi <sunsi@SunSideMac-mini.local>
2016-08-21 22:50:01 +0800

    UIButton中间的显示 tabBar
    UIButton中间的显示 tabBar

--- DSWeibo/scripts/Main/DSMainTabBarController.swift
+++ DSWeibo/scripts/Main/DSMainTabBarController.swift
@@ -13,6 +13,20 @@ class DSMainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tabBar.tintColor=UIColor.orangeColor();
+        
+            /**  添加tabBar 按钮 */
+        addTabBarItemView();
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated);
+        
+        // 添加加号按钮
+        setupComposeBtn()
+    }
+    
+    /**  添加tabBar 按钮 */
+    private func addTabBarItemView(){
         //1.nsBundle加载   2.得到jsonData  3do catch中 解析json
         let path=NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil);
         if let jsonPath=path{
@@ -27,14 +41,22 @@ class DSMainTabBarController: UITabBarController {
                 print(error);
                 addChildViewController("DSHomeTableViewController",title: "首页",imageName: "tabbar_home");
                 addChildViewController("DSMessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+                addChildViewController("NullViewController", title: "", imageName: "")
                 addChildViewController("DSDiscoverTableViewController", title:"广场", imageName: "tabbar_discover")
                 addChildViewController("DSProfileTableViewController", title: "我", imageName: "tabbar_profile");
             }
         }
     }
     
-
-    
+    /** 添加加号按钮  */
+    private func setupComposeBtn(){
+        // 1.添加加号按钮
+        tabBar.addSubview(composeBtn)//先add +号
+        
+        let width=UIScreen.mainScreen().bounds.size.width / CGFloat(viewControllers!.count);
+        let rect=CGRect(x: 0, y: 0, width: width, height: 49);
+        composeBtn.frame=CGRectOffset(rect, 2*width, 0);
+    }
     
     override func didReceiveMemoryWarning() {        super.didReceiveMemoryWarning()    }
     
@@ -68,7 +90,24 @@ class DSMainTabBarController: UITabBarController {
         addChildViewController(nav);
     }
     
+    /**  懒加载 UIButton 2.button的写法*/
+    private lazy var composeBtn:UIButton={
+        let btn=UIButton()
+        btn.setImage(UIImage(named:"tabbar_compose_icon_add"),forState:UIControlState.Normal);
+        btn.setImage(UIImage(named:"tabbar_compose_icon_add_highlighted"), forState: UIControlState.Highlighted)
+
+        btn.setBackgroundImage(UIImage(named:"tabbar_compose_button"), forState: UIControlState.Normal);
+        btn.setBackgroundImage(UIImage(named:"tabbar_compose_button_highlighted"), forState: UIControlState.Highlighted);
+
+        // 4.添加监听
+        btn.addTarget(self, action: #selector(DSMainTabBarController.composeBtnClick), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        return btn;
+    }()
     
+    func composeBtnClick(){
+        print(#function)
+    }
 }
 
 
